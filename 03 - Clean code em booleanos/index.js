655f50d21e022168b6e12f64aeb41842ff58ccd5
weslley <dev.wfeduardo@gmail.com>
2024-04-01 15:54:37 -0300

    03 - Clean code em booleanos + duvidas

---
+++ 03 - Clean code em booleanos/index.js
@@ -0,0 +1,22 @@
+// Clean code em booleanos
+   
+// Se escreve como se fosse uma pergunta: is, does, has
+
+const userMajority = true
+const isUserMajority = true
+
+const disabled = true
+const isDisabled = true
+
+const userHasGroup = true
+
+// 'se desabilitado'
+if (disabled) {}
+
+// 'se está desabilitado'
+if (isDisabled) {}
+
+// --------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------
